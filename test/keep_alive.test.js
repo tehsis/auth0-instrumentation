c071f13a721882717da4b7e0db227e84bbf51734
Yamil Asusta <hello@yamilasusta.com>
2017-11-24 11:14:29 -0400

    Fixes uncaught of wrong agent

--- test/keep_alive.test.js
+++ test/keep_alive.test.js
@@ -4,9 +4,14 @@ const assert = require('assert');
 
 const keepAliveAgentRegistry = require('../lib/keep_alive_agent');
 
-describe('keepAliveAgentRegistry', function() {
-  it('should ', function() {
+describe.only('keepAliveAgentRegistry', function() {
+  it('should generate a http agent', function() {
     const agent = keepAliveAgentRegistry({});
     assert(agent === keepAliveAgentRegistry({ NODE_ENV: 'test'} ));
+  });
+
+  it('should generate an https agent', function() {
+    const agent = keepAliveAgentRegistry({ });
+    assert(agent !== keepAliveAgentRegistry({ NODE_ENV: 'production'} ));
   });
 });
