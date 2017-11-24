c800c1f36d09a5c15a59654b3d6c39c7ab8b78f0
Yamil Asusta <hello@yamilasusta.com>
2017-11-24 11:18:03 -0400

    Add additional asserts

--- test/keep_alive.test.js
+++ test/keep_alive.test.js
@@ -4,14 +4,17 @@ const assert = require('assert');
 
 const keepAliveAgentRegistry = require('../lib/keep_alive_agent');
 
-describe.only('keepAliveAgentRegistry', function() {
+describe('keepAliveAgentRegistry', function() {
   it('should generate a http agent', function() {
     const agent = keepAliveAgentRegistry({});
     assert(agent === keepAliveAgentRegistry({ NODE_ENV: 'test'} ));
+    assert(agent.protocol === 'http:');
   });
 
   it('should generate an https agent', function() {
     const agent = keepAliveAgentRegistry({ });
-    assert(agent !== keepAliveAgentRegistry({ NODE_ENV: 'production'} ));
+    const httpsAgent = keepAliveAgentRegistry({ NODE_ENV: 'production'} );
+    assert(agent !== httpsAgent);
+    assert(httpsAgent.protocol === 'https:');
   });
 });
