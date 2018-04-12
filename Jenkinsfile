75b2f4bf611de4dbc30821ee11ccbefcdef5d378
Hernan Meydac <hernan.meydac@auth0.com>
2018-04-12 18:13:59 -0300

    Adds Jenkinsfile to publish library to npm

---
+++ Jenkinsfile
@@ -0,0 +1,49 @@
+pipeline {
+  agent {
+    label 'crew-brokkr'
+  }
+
+  tools {
+    nodejs '6.10.3'
+  }
+
+  environment {
+    NPM_TOKEN = credentials('auth0npm-npm-token')
+  }
+
+  options {
+    timeout(time: 10, unit: 'MINUTES')
+  }
+
+  parameters {
+    string(name: 'SlackTarget', defaultValue: '#domain-obs-build', description: 'Target Slack Channel for notifications')
+  }
+
+  stages {
+    stage('SharedLibs') { // Required. Stage to load the Auth0 shared library for Jenkinsfile
+      steps {
+        library identifier: 'auth0-jenkins-pipelines-library@master', retriever: modernSCM(
+          [$class: 'GitSCMSource',
+          remote: 'git@github.com:auth0/auth0-jenkins-pipelines-library.git',
+          credentialsId: 'auth0extensions-ssh-key'])
+      }
+    }
+    stage('Build') {
+      steps {
+        sh 'npm install'
+      }
+    }
+    stage('Publish') {
+      steps {
+        sh 'tools/npm.sh'
+      }
+    }
+  }
+
+  post {
+    always {
+      notifySlack(params.SlackTarget);
+      deleteDir()
+    }
+  }
+}
---
+++ tools/npm.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+MATCHER=${2:-"*"}
+NPM_TAG=${3:-"beta"}
+
+NPM_NAME=$(node tools/attribute.js name)
+VERSION=$(node tools/attribute.js version)
+
+NPM_BIN=$(npm bin)
+STABLE=$($NPM_BIN/semver $VERSION -r "*")
+
+# Enable failing on exit status here because semver exits with 1 when the range
+# doesn't match.
+set -e
+
+new_line()
+{
+  echo ""
+}
+
+verbose()
+{
+  echo -e " \033[36m→\033[0m $1"
+}
+
+verbose_item()
+{
+  echo -e " \033[96m∙\033[0m $1"
+}
+
+success()
+{
+  echo -e " \033[1;32m✔︎\033[0m $1"
+}
+
+npm_release()
+{
+  verbose "Checking if version $1 of $NPM_NAME is already available in npm…"
+
+  NPM_EXISTS=$(npm info -s $NPM_NAME@$1 version)
+
+  if [ ! -z "$NPM_EXISTS" ] && [ "$NPM_EXISTS" == "$1" ]; then
+    verbose "There is already a version $NPM_EXISTS in npm. Skipping npm publish…"
+  else
+    if [ ! -z "$STABLE" ]; then
+      verbose "Deploying $1 to npm"
+      npm publish
+    else
+      verbose "Deploying $1 to npm with tag $NPM_TAG"
+      npm publish --tag "$NPM_TAG"
+    fi
+    success "$NPM_NAME uploaded to npm registry"
+  fi
+}
+
+npm_release "$VERSION"
