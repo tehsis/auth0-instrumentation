34faf7e8b0ec0cb3d19d1532f0c36819efd98635
Hernan Meydac <hernan.meydac@auth0.com>
2018-08-27 14:06:46 -0300

    Adds tests for the webErrorHandler function

--- test/logger-streams/error-handlers.test.js
+++ test/logger-streams/error-handlers.test.js
@@ -36,3 +36,38 @@ describe('kinesisErrorHandler', () => {
     afterEach(() => console.error.restore());
   });
 });
+
+describe('webErrorHandler', () => {
+  describe('handle error', () => {
+    const err = { message: 'test error', records: {}, stack: {} };
+    const expectedMessage = JSON.stringify({
+      message: err.message,
+      stack: err.stack
+    });
+
+
+    beforeEach(() => {
+      const handler = require('../../lib/logger-streams/error-handlers').webErrorHandler;
+
+      sinon.stub(console, 'error');
+      handler(err);
+    });
+
+    it('should call console.error once', () => assert.isTrue(console.error.calledOnce));
+    it('should call console.error with error message', () => assert.isTrue(console.error.calledWith('Error on writing logs to web url', expectedMessage)));
+    afterEach(() => console.error.restore());
+  });
+
+  describe('without err object', () => {
+    beforeEach(() => {
+      const handler = require('../../lib/logger-streams/error-handlers').webErrorHandler;
+
+      sinon.stub(console, 'error');
+      handler();
+    });
+
+    it('should call console.error once', () => assert.isTrue(console.error.calledOnce));
+    it('should call console.error with error message', () => assert.isTrue(console.error.calledWith('Error on writing logs to web url')));
+    afterEach(() => console.error.restore());
+  });
+});
