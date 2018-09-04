1654f260b334cc0d9a348f3741ce9b9a802f5794
Jon Allie <jon@jonallie.com>
2018-09-04 15:00:03 -0400

    Improve tests and avoid flakiness (#119)
    * Improve profiler tests (which are currently flaky in the CI)
    
     - Retry the snapshot test to avoid flakiness caused by slow snapshots or file writes.
     - Propagate errors to 'done' so that failures don't show up as timeouts.
     - Some misc cleanup while I'm here.

--- test/profiler.test.js
+++ test/profiler.test.js
@@ -1,7 +1,7 @@
-var assert = require('assert');
-var sinon = require('sinon');
+const assert = require('assert');
+const sinon = require('sinon');
 
-var Profiler = require('../lib/profiler');
+const Profiler = require('../lib/profiler');
 
 describe('Profiler', function() {
   var profiler, agent;
@@ -16,20 +16,27 @@ describe('Profiler', function() {
     };
     profiler = new Profiler(agent, { name: 'test' }, { HUNT_MEMORY_LEAKS: true });
     // avoid having to create workers
-    process.send = function() {};
+    sinon.replace(process, 'send', sinon.fake());
   });
 
   afterEach(function() {
-    process.send = undefined;
+    sinon.restore();
   });
 
   describe('#createThrottledSnapshot', function() {
-    it('should create a snapshot and log', function(done) {
+    it('should create a snapshot and report', function(done) {
+      // this test can be flaky, due to slowness in writing or snapshotting.
+      this.retries(3);
+      sinon.replace(profiler, 'report', sinon.spy());
       this.timeout(3000);
       profiler.createThrottledSnapshot('testing');
       setTimeout(() => {
-        assert(agent.logger.info.calledOnce);
-        done();
+        try {
+          assert(profiler.report.calledOnceWith(sinon.match.defined, sinon.match('testing')));
+          done();
+        } catch (err) {
+          done(err);
+        }
       }, 1000);
     });
   });
@@ -38,9 +45,13 @@ describe('Profiler', function() {
     it('should create a profile', function(done) {
       this.timeout(3000);
       profiler.createProfile(1000, function(err, path) {
-        assert.ifError(err);
-        assert(path);
-        done();
+        try {
+          assert.ifError(err);
+          assert(path);
+          done();
+        } catch (err) {
+          done(err);
+        }
       });
     });
   });
