8f8f13602839c464087ebde573d725805a38717e
Santiago Aguiar <santiago.aguiar@auth0.com>
2018-10-02 13:39:25 -0300

    Fix profile GC

--- test/profiler.test.js
+++ test/profiler.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const sinon = require('sinon');
-
+var $require = require('proxyquire').noPreserveCache();
 const Profiler = require('../lib/profiler');
 
 describe('Profiler', function() {
@@ -11,7 +11,8 @@ describe('Profiler', function() {
         histogram: sinon.spy()
       },
       logger: {
-        info: sinon.spy()
+        info: sinon.spy(),
+        error: sinon.spy()
       }
     };
     profiler = new Profiler(agent, { name: 'test' }, { HUNT_MEMORY_LEAKS: true });
@@ -52,6 +53,61 @@ describe('Profiler', function() {
         } catch (err) {
           done(err);
         }
+      });
+    });
+  });
+
+  describe('#setupGCReporter', () => {
+    const EventEmitter = require('events');
+    const stats = new EventEmitter();
+    function getProfiler(env) {
+      const Profiler = $require('../lib/profiler', {
+        'gc-stats': () => stats
+      });
+
+      return new Profiler(agent, { name: 'test' }, env);
+    }
+
+    describe('when there is a new GC stat', () => {
+      let clock;
+      beforeEach(() => {
+        clock = sinon.useFakeTimers(new Date(), 'setTimeout');
+      });
+
+      afterEach(() => {
+        clock.restore();
+      });
+
+      it ('does not takes snapshot', () => {
+        const profiler = getProfiler({ PROFILE_GC: true });
+        profiler.setupGCReporter();
+
+        const createSnapshotStub = sinon.stub(profiler, 'createSnapshot');
+        stats.emit('stats', { pauseMS: 1000 });
+        clock.tick(6000);
+        sinon.assert.notCalled(createSnapshotStub);
+      });
+
+      describe('and HUNT_LONG_GC is set', () => {
+        it('does not take snapshot if pause <= 500', () => {
+          const profiler = getProfiler({ PROFILE_GC: true, HUNT_LONG_GC: true });
+          profiler.setupGCReporter();
+          stats.emit('stats', { pauseMS: 500 });
+          clock.tick(6000);
+          const createSnapshotStub = sinon.stub(profiler, 'createSnapshot');
+          sinon.assert.notCalled(createSnapshotStub);
+        });
+
+        it('takes snapshot if pause > 500', () => {
+          const profiler = getProfiler({ PROFILE_GC: true, HUNT_LONG_GC: true });
+          const createSnapshotStub = sinon.stub(profiler, 'createSnapshot');
+
+          profiler.setupGCReporter();
+          stats.emit('stats', { pauseMS: 501 });
+          clock.tick(6000);
+          sinon.assert.calledOnce(createSnapshotStub);
+          sinon.assert.calledWith(createSnapshotStub, 'LONG_GC_PAUSE');
+        });
       });
     });
   });
